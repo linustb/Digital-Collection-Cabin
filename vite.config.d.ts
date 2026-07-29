674671a7a6728db7777331ecaf532e5b5f800146
linustb <2573281290@qq.com>
2026-07-29 11:08:45 +0800

    Update vite.config.d.ts

--- vite.config.d.ts
+++ vite.config.d.ts
@@ -1,2 +1,7 @@
-declare const _default: import("vite").UserConfig;
-export default _default;
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/Digital-Collection-Cabin/',
+})
